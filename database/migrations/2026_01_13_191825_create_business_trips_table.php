2867a8025508b03d4fa3ecdb0039c13e1f53ac8b
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2026-01-13 23:45:36 +0300

    feat: create business_trips table

---
+++ database/migrations/2026_01_13_191825_create_business_trips_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('business_trips', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained('users');
+            $table->foreignId('car_id')->constrained();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->enum('status', ['planned', 'in_progress', 'completed', 'cancelled']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('business_trips');
+    }
+};
