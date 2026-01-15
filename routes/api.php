03a98e2ea42e2656933ff58fb7a9caa3f4d23dc9
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2026-01-16 01:42:32 +0300

    refactor: add /trips, /available-car api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CarController;
+use App\Http\Controllers\Api\BusinessTripController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/trips', [BusinessTripController::class, 'index']);
+
+Route::get('/available-car', [CarController::class, 'available'])->middleware('auth:sanctum');
+
