afb48716ef874c5a41cab22491e1b0fd383b68c4
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2026-01-15 11:52:51 +0300

    refactoring: create employees with all positions and 15 drivers

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Role;
+use App\Models\Position;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,20 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(50)->create();
+        $employeeRoleId = Role::where('name', 'employee')->first()->id;
+        $positions = Position::all()->toArray();
+
+        foreach($positions as $position) {
+            $name = explode(' ', strtolower($position['name']))[0];
+            User::factory()->create([
+                'name' => $name,
+                'role_id' => $employeeRoleId,
+                'position_id' => $position['id'],
+                'email' => $name . '@example.com',
+                'password' => '1234567890',
+            ]);
+        }
+
+        User::factory(15)->driver()->create();
     }
 }
